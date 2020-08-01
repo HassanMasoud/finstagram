39c1d7b9de5c8202b8dc810c727ad4518facace0
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-01 14:52:05 +0300

    Render the images on the page

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,7 +1,17 @@
 import React from "react";
+import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
-  return <div className="image-grid">Images</div>;
+  const { docs } = useFirestore("images");
+
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <img src={doc.url} key={doc.id} alt="uploaded pic" />
+        ))}
+    </div>
+  );
 };
 
 export default ImageGrid;
